eddd8276701554b8aff1411a1cde8e7d626b680e
tom <thamaraiselvam@live.com>
2023-02-03 14:49:08 +0800

    Add Log4j for logging json

--- src/employeeservice/src/main/java/com/example/employeeservice/controller/EmployeeController.java
+++ src/employeeservice/src/main/java/com/example/employeeservice/controller/EmployeeController.java
@@ -4,50 +4,56 @@ import java.util.List;
 
 import com.example.employeeservice.model.Employee;
 import com.example.employeeservice.repository.EmployeeRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.StringMapMessage;
 
 @RestController
 public class EmployeeController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
+//    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
+    private static final Logger log = LogManager.getLogger(EmployeeController.class);
 
     @Autowired
     EmployeeRepository repository;
 
     @PostMapping("/")
     public Employee add(@RequestBody Employee employee) {
-        LOGGER.info("Employee add: {}", employee);
+        log.info("Employee add: {}", employee);
+        log.info(new StringMapMessage()
+                .with("message", "Employee add")
+                .with("endpoint", "/")
+                .with("employee", employee));
         return repository.save(employee);
     }
 
     @GetMapping("/{id}")
     public Employee findById(@PathVariable("id") String id) {
-        LOGGER.info("Employee find: id={}", id);
+        log.info("Employee find: id={}", id);
         return repository.findById(id).get();
     }
 
     @GetMapping("/")
     public Iterable<Employee> findAll() {
-        LOGGER.info("Employee find");
+        log.info("Employee find");
         return repository.findAll();
     }
 
     @GetMapping("/department/{departmentId}")
     public List<Employee> findByDepartment(@PathVariable("departmentId") String departmentId) {
-        LOGGER.info("Employee find: departmentId={}", departmentId);
+        log.info("Employee find: departmentId={}", departmentId);
         return repository.findByDepartmentId(departmentId);
     }
 
     @GetMapping("/organization/{organizationId}")
     public List<Employee> findByOrganization(@PathVariable("organizationId") String organizationId) {
-        LOGGER.info("Employee find: organizationId={}", organizationId);
+        log.info("Employee find: organizationId={}", organizationId);
         return repository.findByOrganizationId(organizationId);
     }
 
